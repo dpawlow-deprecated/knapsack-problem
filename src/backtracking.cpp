14889b951ab9d9ce8c0c9a43e3f80851df44fd25
Dante <dante.pawlow@gmail.com>
2018-03-31 16:13:25 -0300

    Backtracking sin podas

---
+++ src/backtracking.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct item {
+    int size;
+    int value;
+};
+
+struct backpack {
+    int size;
+    int load;
+    int value;
+    vector<item> items;
+};
+
+backpack backtracking(int i, backpack bkp, vector<item> const &items) {
+    if (i >= items.size()) {
+        return bkp;
+    }
+    backpack backpackWithoutItem = backtracking(i+1, bkp, items);
+
+    if (bkp.load + items[i].size > bkp.size) {
+        return backpackWithoutItem;
+    }
+
+    bkp.load += items[i].size;
+    bkp.value += items[i].value;
+    bkp.items.push_back(items[i]);
+    backpack backpackWithItem = backtracking(i+1, bkp, items);
+
+    if (backpackWithItem.value > backpackWithoutItem.value) {
+        return backpackWithItem;
+    } else {
+        return backpackWithoutItem;
+    }
+}
+
+backpack solve(int bkpSize, vector<item> const &items) {
+    backpack bkp;
+    bkp.value = 0;
+    bkp.load = 0;
+    bkp.size = bkpSize;
+
+    return backtracking(0, bkp, items);
+};
+
+
+int main() {
+    item item1; item item2; item item3; item item4; item item5; item itemA; item itemB;
+    itemA.size = 15; itemA.value = 2;
+    itemB.size = 15; itemB.value = 2;
+    item1.size = 3; item1.value = 10;
+    item2.size = 4; item2.value = 10;
+    item3.size = 1; item3.value = 100;
+    item4.size = 10; item4.value = 20;
+    item5.size = 5; item5.value = 15;
+
+    vector<item> items = {itemA, item1, item2, item3, item4, item5, itemB};
+
+    backpack bkp = solve(15, items);
+
+    cout<<bkp.value<<endl;
+    cout<<bkp.load;
+
+    return 1;
+}
---
+++ src/bruteforce.cpp
@@ -0,0 +1,21 @@
+/* #include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+struct item {
+    int size;
+    int value;
+};
+
+vector<item> solve(int bkpSize, vector<item> const &items) {
+
+};
+
+
+int main() {
+    return 1;
+}
+
+*/
